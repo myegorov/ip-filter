6711e73eb5c7c6d2b0fbb16aad16f20be00a8946
Maksim Yegorov <findmaksim@gmail.com>
2018-03-29 00:35:51 -0400

    added bitarray & FNV hash; todo: prototype Bloom filter

---
+++ prototype/fnv.py
@@ -0,0 +1,16 @@
+'''
+64-bit Fowler-Noll-Vo (FNV1a) hash function:
+https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+'''
+import getsizeof from sys
+
+FNV_OFFSET_BASIS = 0xcbf29ce484222325
+FNV_PRIME = 0x100000001b3
+
+def hash_fnv(num):
+    res = FNV_OFFSET_BASIS
+    for i in range(getsizeof(num)): # for each byte
+        byte_chunk = (num>>(8*i)) & 0xff # extract byte
+        res ^= byte_chunk
+        res *= FNV_PRIME
+    return res % 2**64
